ee1c51c2200a3789ea1376ccab9a6032b3d537a1
Fernando Camargo <camargodelbuono@gmail.com>
2018-03-14 01:08:58 +0100

    Remove unnecessary helpers

--- src/sum.js
+++ src/sum.js
@@ -1,23 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const isNumber = object => !isNaN(Number(object));
-
 const accumulate = (previous, next) => Number(previous) + Number(next);
 
 const add = (...numbers) => numbers.filter(Number).reduce(accumulate, 0);
 
-const read = file => fs.readFileSync(file, 'utf8');
-
-const exist = file => fs.existsSync(file);
-
-const load = file => exist(file) && read(file);
+const load = file => fs.existsSync(file) && fs.readFileSync(file, 'utf8');
 
 const dig = file => sum(file);
 
 const parse = file => (stack, line) => {
   const nested = path.join(path.dirname(file), line);
-  const deep = exist(nested) && dig(nested);
+  const deep = fs.existsSync(nested) && dig(nested);
 
   return Object.assign(stack, deep, {
     [file]: add(stack[file], line, deep[nested]),
